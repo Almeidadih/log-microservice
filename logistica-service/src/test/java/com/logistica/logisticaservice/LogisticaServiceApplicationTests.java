4d7d43a48a33eccd550a0221baf8f80ac368860c
diego <almeidadieo488@gmail.com>
2026-09-07 18:19:07 -0300

    Initial commit

---
+++ logistica-service/src/test/java/com/logistica/logisticaservice/LogisticaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistica.logisticaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
